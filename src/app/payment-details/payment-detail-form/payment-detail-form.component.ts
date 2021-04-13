301607db61e0dedf2fe2e50e7556d96d84bb2252
makireland <makireland@gmail.com>
2021-04-13 23:02:13 +0100

    setting the api and created functions

--- src/app/payment-details/payment-detail-form/payment-detail-form.component.ts
+++ src/app/payment-details/payment-detail-form/payment-detail-form.component.ts
@@ -1,16 +1,33 @@
-import { Component, OnInit } from '@angular/core';
-import { PaymentDetailService } from 'src/app/shared/payment-detail.service';
+import {
+  Component,
+  OnInit
+} from '@angular/core';
+import {
+  NgForm
+} from '@angular/forms';
+import {
+  PaymentDetailService
+} from 'src/app/shared/payment-detail.service';
 
 @Component({
   selector: 'app-payment-detail-form',
   templateUrl: './payment-detail-form.component.html',
-  styles: [
-  ]
+  styles: []
 })
 export class PaymentDetailFormComponent implements OnInit {
 
-  constructor(public service:PaymentDetailService) { }
+  constructor(public service: PaymentDetailService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onSubmit(form: NgForm) {
+    this.service.postPaymentDetail().subscribe(
+      res => {
+
+      },
+      err => {
+        console.log(err);
+      }
+    );
   }
 }
