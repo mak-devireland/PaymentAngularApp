a99528a3f39fec5f0a4b2f39482e42830988ef7a
makireland <makireland@gmail.com>
2021-04-17 10:09:45 +0100

    applied a succes message and get call back list from database

--- src/app/payment-details/payment-detail-form/payment-detail-form.component.ts
+++ src/app/payment-details/payment-detail-form/payment-detail-form.component.ts
@@ -5,6 +5,8 @@ import {
 import {
   NgForm
 } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { PaymentDetail } from 'src/app/shared/payment-detail.model';
 import {
   PaymentDetailService
 } from 'src/app/shared/payment-detail.service';
@@ -16,18 +18,25 @@ import {
 })
 export class PaymentDetailFormComponent implements OnInit {
 
-  constructor(public service: PaymentDetailService) {}
+  constructor(public service: PaymentDetailService, private toastr:ToastrService) {}
 
   ngOnInit(): void {}
 
   onSubmit(form: NgForm) {
     this.service.postPaymentDetail().subscribe(
       res => {
-
+        this.resetForm(form);
+        this.toastr.success('Submit successfully', 'Payment Details Register');
       },
       err => {
         console.log(err);
       }
     );
   }
+
+  resetForm(form: NgForm){
+    form.form.reset();
+    this.service.formData = new PaymentDetail();
+  }
+
 }
